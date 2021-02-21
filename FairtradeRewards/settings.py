aa81093d719053eb2d734f19b328fa75f7d08155
John Jamora <769-jjamora@users.noreply.gitlab1.its.sfu.ca>
2021-02-21 00:50:40 -0800

    Made logo clickable

--- FairtradeRewards/settings.py
+++ FairtradeRewards/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'aih003hf_1*gn=%n03$lkyo#r9g3=ap8gcj!ul7uilan4-i-de'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["fairtrade-rewards.herokuapp.com", "127.0.0.1", "localhost"]
 
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
